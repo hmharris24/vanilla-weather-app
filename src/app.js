767e8c2d45d06b19baed122da28dd952431ac9c5
hmharris24 <102563834+hmharris24@users.noreply.github.com>
2022-05-19 20:15:57 -0500

    Removed console.log

--- src/app.js
+++ src/app.js
@@ -67,7 +67,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "dc32ae2fc1cb2115d010e3e5c4d4cc72";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
